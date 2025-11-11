aa6a3add3f0152501605528e9c215a65489fb9d2
drobotk <pawwwll@gmail.com>
2025-11-11 15:11:22 +0100

    fix: add stub to fix CI

---
+++ playback/src/playplay.cpp
@@ -0,0 +1,11 @@
+#include <stdint.h>
+
+extern "C" {
+    int32_t playplay_get_version() {
+        return -1;
+    }
+    void playplay_get_key(uint8_t buffer[16]) {
+    }
+	void playplay_decrypt(uint8_t key[16], uint8_t fileId[16], uint8_t outputBuffer[16]) {
+	}
+}
